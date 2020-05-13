116265b6aaacd72ea824251864b2fbcff08dcbfe
Karl Marx Manzano <karlmarxmanzano@gmail.com>
2020-05-13 19:17:21 +0800

    Update UI design

--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="col-md-12">
+<div class="row justify-content-center">
+  <div class="col-md-8">
 		@if (session('status'))
 		    <div class="alert alert-success">
 		        {{ session('status') }}
@@ -17,5 +18,6 @@
 				</form>
 			</div>
 		</div>
-	</div>
+  </div>
+</div>
 @endsection
--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -3,18 +3,21 @@
 @section('content')
 <div class="row justify-content-center">
   <div class="col-md-8">
-      <div class="card">
-      <div class="card-header">{{ $contact->first_name }} {{ $contact->last_name }}</div>
-      <p>{{ $contact->email }}</p>
-      <p>{{ $contact->contact_number }}</p>
-      <a href="{{ route('contacts.edit', $contact) }}">Edit</a>
-      <a href="" class="btn btn-outline-danger btn-sm" role="button" onclick="event.preventDefault(); document.getElementById('delete-contact').submit();">
-        Delete
-      </a>
-      <form id="delete-contact" action="{{ route('contacts.destroy', $contact) }}" method="post" style="display: none;">
-        @csrf
-        @method('DELETE')
-    </form>
+    <div class="card mb-3">
+      <div class="card-body">
+        <h5 class="card-title">{{ $contact->first_name }} {{ $contact->last_name }}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">{{ $contact->email }}</h6>
+        <p class="card-text">{{ $contact->contact_number }}</p>
+        <a href="{{ route('contacts.show', $contact) }}">View</a>
+        <a href="{{ route('contacts.edit', $contact) }}">Edit</a>
+        <a href="" class="btn btn-outline-danger btn-sm" role="button" onclick="event.preventDefault(); document.getElementById('delete-contact').submit();">
+          Delete
+        </a>
+        <form id="delete-contact" action="{{ route('contacts.destroy', $contact) }}" method="post" style="display: none;">
+          @csrf
+          @method('DELETE')
+        </form>
+      </div>
     </div>
   </div>
 </div>
